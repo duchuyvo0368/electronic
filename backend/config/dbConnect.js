2e48d9f5ea0d4ba19f0ae8a7ed5e0fc478280e58
duchuyvo0368 <voduchuy016688@gmail.com>
2024-12-05 16:48:56 +0700

    docke7

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,11 +1,15 @@
 const { default: mongoose } = require("mongoose");
 
-const dbConnect = () => {
+const dbConnect = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGODB_URL);
-    console.log("Connect database successful");
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database connected successfully: ${conn.connection.host}`);
   } catch (error) {
-    console.log("Database error");
+    console.error(`Database connection error: ${error.message}`);
+    process.exit(1); 
   }
 };
 
